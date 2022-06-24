dd162a2f1eb1878d5a4663435dccc95a056544ee
project <zimzimrick@gmail.com>
2022-06-24 10:25:33 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,52 @@
+// import { StyleSheet, Text, View, Dimensions } from 'react-native'
+// import React from 'react'
+// import RoootNavigator from './src/navigations/RootNavigator'
+
+
+
+
+// const App = () => {
+//   return (
+//     <RoootNavigator />
+//   )
+// }
+
+// export default App
+
+// const styles = StyleSheet.create({
+
+//   container:{
+//     flex:1
+//   }  
+  
+// });
+
+
+
+import React from 'react'
+import { StyleSheet, Text, View,Dimensions } from 'react-native'
+import { OriginContextProvider,DestinationContextProvider } from './src/contexts/contexts'
+import RoootNavigator from './src/navigations/RootNavigator'
+
+
+const App = () => {
+  return (
+  <DestinationContextProvider>
+    <OriginContextProvider>
+        <RoootNavigator />
+    </OriginContextProvider>
+   </DestinationContextProvider>
+   
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+
+container:{
+  flex:1
+}
+
+
+})
